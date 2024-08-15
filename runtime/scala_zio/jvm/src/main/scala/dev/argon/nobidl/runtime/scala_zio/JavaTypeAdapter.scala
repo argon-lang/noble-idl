8cc3be201682e77f1ac4331b4d5f6bdb85d5772c
argon-dev <argon@argon.dev>
2024-08-15 09:48:15 -0500

    Remove old emit code.

--- runtime/scala_zio/jvm/src/main/scala/dev/argon/nobidl/runtime/scala_zio/JavaTypeAdapter.scala
+++
@@ -1,9 +0,0 @@
-package dev.argon.nobidl.runtime.scala_zio
-
-import scala.jdk.CollectionConverters.*
-import scala.jdk.OptionConverters.*
-
-trait JavaTypeAdapter[A, B] {
-  def toJava(a: A): B
-  def fromJava(b: B): A
-}
